b3b18567147ed696a04ce9056cb4998979d7a87a
vidrepar <vid.repar@gmail.com>
2016-07-06 19:57:10 +0200

    Added build process in gulpfile; Added readme; Some fixes here and there

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,48 @@
-var gulp = require('gulp');
-var serve = require('gulp-serve');
+var gulp        = require('gulp');
+var serve       = require('gulp-serve');
+var domSrc      = require('gulp-dom-src');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var cheerio     = require('gulp-cheerio');
+var htmlMin     = require('gulp-htmlmin');
+var cssmin      = require('gulp-cssmin');
+var autoprefixer= require('gulp-autoprefixer');
 
 gulp.task('serve', serve('./'));
+
+gulp.task('css', function () {
+
+    domSrc({ file:'index.html', selector:'link', attribute:'href' })
+        .pipe(concat('app.full.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/'))
+
+});
+
+gulp.task('js', function () {
+
+    domSrc({ file:'index.html', selector:'script', attribute:'src' })
+        .pipe(concat('app.full.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/'));
+
+});
+
+gulp.task('html', function () {
+
+    gulp.src('index.html')
+        .pipe(cheerio(function ($) {
+
+            $('link').remove();
+            $('script').remove();
+
+            $('head').append('<link rel="stylesheet" href="app.full.min.css">');
+            $('body').append('<script src="app.full.min.js"></script>');
+
+        }))
+        .pipe(htmlMin({ collapseWhitespace:true }))
+        .pipe(gulp.dest('dist/'));
+
+});
+
+gulp.task('build', ['css', 'js', 'html']);
---
+++ readme.md
@@ -0,0 +1,31 @@
+#1. Get the template
+
+```
+git init
+```
+
+```
+git pull https://github.com/vidrepar/creative-coding-template.git
+```
+
+#2. Get the dependencies
+
+```
+npm i
+```
+
+```
+bower i
+```
+
+#3. Serve
+
+```
+gulp serve
+```
+
+#4. Build
+
+```
+gulp build
+```
